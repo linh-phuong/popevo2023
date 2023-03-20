57188def73f1681fe904690fbf7d67916415d572
Linh-Phuong <linhphuong89@gmail.com>
2023-03-20 22:24:18 +0100

    update replicator equation

--- dynamical_system.py
+++ dynamical_system.py
@@ -10,18 +10,22 @@ def structural_population(n, leslie_matrix):
     return leslie_matrix @ n
 
 
-# def replicator_exp(t, n, r):
-#     n_total = n[-1]
-#     freq = n[:-1]
-#     r_bar = sum(r * freq)
-#     return list((r - r_bar) * freq) + [r_bar * n_total]
-
-
 def replicator_niche_overlap(t, n, pars):
     r = pars[:-1]
     alpha = pars[-1]
     n_total = n[-1]
     freq = n[:-1]
-    growth = r - alpha * n_total
-    r_bar = sum(growth * freq)
-    return list((growth - r_bar) * freq) + [r_bar * n_total]
+    gr = r - alpha * n_total
+    r_bar = sum(gr * freq)
+    return list((gr - r_bar) * freq) + [r_bar * n_total]
+
+
+def replicator(t, n, pars):
+    nb_sp = len(n) - 1
+    freq = n[0:nb_sp]
+    ntotal = n[-1]
+    r = pars[0:nb_sp]
+    A = pars[nb_sp:].reshape(nb_sp, nb_sp)
+    gr = r - A@freq
+    gr_bar = sum(gr * freq)
+    return list((gr - gr_bar) * freq) + [gr_bar * ntotal]
--- replicator_eq_app.py
+++ replicator_eq_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from scipy.integrate import solve_ivp
-from dynamical_system import replicator_niche_overlap
+from dynamical_system import replicator_niche_overlap, replicator
 import numpy as np
 import pandas as pd
 import altair as alt
@@ -11,13 +11,19 @@ st.title("Replicator equation")
 st.header("Exponential growth population")
 
 
-r1 = st.sidebar.slider("Intrinsic growth rate of type 1", 0.0, 3.0, value=2.0, step=0.1)
-r2 = st.sidebar.slider("Intrinsic growth rate of type 2", 0.0, 3.0, value=2.2, step=0.1)
-r3 = st.sidebar.slider("Intrinsic growth rate of type 3", 0.0, 3.0, value=1.8, step=0.1)
+r1 = st.sidebar.slider("Intrinsic growth rate of type 1",
+                       0.0, 3.0, value=2.0, step=0.1)
+r2 = st.sidebar.slider("Intrinsic growth rate of type 2",
+                       0.0, 3.0, value=2.2, step=0.1)
+r3 = st.sidebar.slider("Intrinsic growth rate of type 3",
+                       0.0, 3.0, value=1.8, step=0.1)
 
-init = np.array([0.1 / 0.3, 0.1 / 0.3, 0.1 / 0.3, 0.3])
+ntotal = 0.3
+init = np.array([0.1 / ntotal, 0.1 / ntotal, 0.1 / ntotal, ntotal])
+
 pars = np.array([r1, r2, r3, 0])
-exp_sol = solve_ivp(replicator_niche_overlap, (0, 50), init, args=(pars,))
+exp_sol = solve_ivp(replicator_niche_overlap, (0, 50), init,
+                    args=(pars,), t_eval=np.arange(0, 50, 0.5))
 
 df_exp = pd.DataFrame(
     dict(
@@ -29,16 +35,13 @@ df_exp = pd.DataFrame(
     )
 )
 
-df_exp["density_type_1"] = df_exp.freq_type_1 * df_exp.total
-df_exp["density_type_2"] = df_exp.freq_type_2 * df_exp.total
-df_exp["density_type_3"] = df_exp.freq_type_3 * df_exp.total
 
 col1, col2 = st.columns(2)
 
 with col1:
     st.altair_chart(
         alt.Chart(df_exp)
-        .transform_fold(["feq_type_1", "freq_type_2", "freq_type_3"])
+        .transform_fold(["freq_type_1", "freq_type_2", "freq_type_3"])
         .mark_line()
         .encode(x="time", y="value:Q", color="key:N")
     )
@@ -46,17 +49,20 @@ with col1:
 with col2:
     st.altair_chart(
         alt.Chart(df_exp)
-        .transform_fold(["denisty_type_1", "density_type_2", "density_type_3", "total"])
+        .transform_fold(["total"])
         .mark_line()
-        .encode(x="time", y=alt.Y("value:Q", scale=alt.Scale(type="log")), color="key:N")
+        .encode(x="time", y=alt.Y("value:Q", scale=alt.Scale(type="log")), color=alt.value("green"))
     )
 
 st.header("Logistic growth without niche difference")
 
-alpha = st.sidebar.slider("Competition coefficient", 0.0, 1.0, value=0.2, step=0.1)
+alpha = st.sidebar.slider("Competition coefficient",
+                          0.0, 1.0, value=0.2, step=0.1)
 
 pars_logi = np.array([r1, r2, r3, alpha])
-logi_sol = solve_ivp(replicator_niche_overlap, (0, 50), init, args=(pars_logi,))
+
+logi_sol = solve_ivp(replicator_niche_overlap,
+                     (0, 50), init, args=(pars_logi,), t_eval=np.arange(0, 50, 0.5))
 
 df_logi = pd.DataFrame(
     dict(
@@ -68,9 +74,46 @@ df_logi = pd.DataFrame(
     )
 )
 
-st.altair_chart(
-    alt.Chart(df_logi)
-    .transform_fold(["feq_type_1", "freq_type_2", "freq_type_3"])
-    .mark_line()
-    .encode(x="time", y="value:Q", color="key:N")
-)
+
+col3, col4 = st.columns(2)
+
+with col3:
+    st.altair_chart(
+        alt.Chart(df_logi)
+        .transform_fold(["freq_type_1", "freq_type_2", "freq_type_3"])
+        .mark_line()
+        .encode(x="time", y="value:Q", color="key:N")
+    )
+
+with col4:
+    st.altair_chart(
+        alt.Chart(df_logi)
+        .transform_fold(["total"])
+        .mark_line()
+        .encode(x="time", y="value:Q", color=alt.value("green"))
+    )
+
+st.header("Asymetric competitions create niche difference")
+
+a11 = st.sidebar.slider(
+    "Intraspecific competition of type 1", 0., 2., value=1., step=0.1)
+a22 = st.sidebar.slider(
+    "Intraspecific competition of type 2", 0., 2., value=1., step=0.1)
+a33 = st.sidebar.slider(
+    "Intraspecific competition of type 3", 0., 2., value=1., step=0.1)
+
+pars_nichediff = np.array(
+    [r1, r2, r3, a11, 0.8, 0.8, 0.8, a22, 0.8, 0.8, 0.8, a33])
+
+nichediff_sol = solve_ivp(replicator, (0, 50), init,
+                          t_eval=np.arange(0, 50, 0.5), args=(pars_nichediff,))
+
+
+df_nd = pd.DataFrame(dict(time=nichediff_sol.t,
+                     freq_type_1=nichediff_sol.y[0, :], freq_type_2=nichediff_sol.y[1, :], freq_type_3=nichediff_sol.y[2, :], total=nichediff_sol.y[3, :]))
+
+
+st.altair_chart(alt.Chart(df_nd).transform_fold(
+    ["freq_type_1", "freq_type_2", "freq_type_3"]).mark_line().encode(x='time', y='value:Q', color='key:N'))
+
+st.write(df_nd.head(10))
